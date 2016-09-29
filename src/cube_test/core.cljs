3b13374e8183986a75bf23f064a2cf9d1b1de294
Mike Belanger <mikebelanger@fastmail.com>
2016-09-29 17:54:38 -0400

    Scene is now animated with simple functions.

--- src/cube_test/core.cljs
+++ src/cube_test/core.cljs
@@ -8,27 +8,76 @@
 (check-version)
 
 (defn ^:export start
-  []
-  (let [m-main    (.require js/b4w "main")
-        m-data    (.require js/b4w "data")
-        m-scene   (.require js/b4w "scenes")
-        m-config  (.require js/b4w "config")
-        m-debug   (.require js/b4w "debug")
-        a-trans   (.require js/b4w "transform")
-        m-camera  (.require js/b4w "camera")
-        canvas    (.getElementById js/document "container")]
+  [json]
+  (let [m-main      (.require js/b4w "main")
+        m-data      (.require js/b4w "data")
+        m-scene     (.require js/b4w "scenes")
+        m-config    (.require js/b4w "config")
+        m-trans     (.require js/b4w "transform")
+        m-camera    (.require js/b4w "camera")
+        m-controls  (.require js/b4w "controls")
+        m-material  (.require js/b4w "material")
+        m-rgba      (.require js/b4w "rgba")
+        canvas      (.getElementById js/document "container")
+        position    (atom {:current 0
+                           :direction 1
+                           :back-limit 30
+                           :front-limit -30})
+        cube-range  (range -30 30)]
 
     (defn loaded-cb [data-id success]
-      (.log js/console "data id, success are: " data-id success))
+      (.log js/console
+            "Main scene loaded on thread number: " data-id
+            "with a success value of: " success))
+
+    ;;You can think this manifold-cb as an update per tick loop.  At least when
+    ;;the manifold type is an elapsed one. Note that the obj parameter returns
+    ;;the time between the last update and the current - in seconds.
+    (defn manifold-cb [obj manifold-id]
+      "Make any per-tick changes here."
+      (let [current-camera (.get-active-camera m-scene)
+            cube           (.get-object-by-name m-scene "Cube")
+            sensor-data    (first obj)
+            mat-name       (first (.get-materials-names m-material cube))]
+
+            (if (or (= (:current @position) (:back-limit @position))
+                    (= (:current @position) (:front-limit @position)))
+                      (swap! position assoc :direction (* -1 (:direction @position))))
+
+            (swap! position assoc :current (+ (:direction @position)
+                                              (:current @position)))
+
+            (.set-diffuse-color m-material cube mat-name
+                                (.from-values m-rgba
+                                            (/ (rand-int 10) 10)
+                                            (/ (rand-int 10) 10)
+                                            (/ (rand-int 10) 10)))
+
+            (.set-translation m-trans cube
+                              0
+                              1
+                              (/ (:current @position) 10))
+
+            (.rotate-y-local m-trans cube sensor-data)))
 
     (defn stageload-cb [data-id success]
-
+      "Use camera, translation and other b4w modules if you want to do something
+      static (ie the whole time its running). If you want something to change
+      over time, invoke the module functions within sensor manifold's callback
+      scope instead.  In this case, that callback function would be manifold-cb"
       (when (.is-primary-loaded m-data)
-                (let [current-camera (.get-active-camera m-scene)
-                      cube           (.get-object-by-name m-scene "Weird")]
+                (let [elapsed-sensor (.create-elapsed-sensor m-controls)]
+                  ;;The manifold sensor can act as an update-tick.
+                      (.create-sensor-manifold m-controls
+                                               nil
+                                               "m_main"
+                                               (.-CT_CONTINUOUS m-controls)
+                                               (clj->js [elapsed-sensor])
+                                                manifold-cb)
 
-                      (.translate-view m-camera current-camera 1 1 -10))))
+                  (.log js/console "stage-load callback finished"))))
 
-    ;;(.set m-config "console_verbose" true)
-    (.load m-data "cube_test.json" loaded-cb stageload-cb true false)
+    (.set m-config "console_verbose" true)
+    (.load m-data (str json) loaded-cb stageload-cb true false)
+
     (.init m-main canvas)))
--- target/main.out/cube_test/core.js
+++ target/main.out/cube_test/core.js
@@ -6,34 +6,64 @@ var m_version = b4w.require("version");
 return console.log("Using Blend4Web Version",m_version.version());
 });
 cube_test.core.check_version.call(null);
-cube_test.core.start = (function cube_test$core$start(){
+cube_test.core.start = (function cube_test$core$start(json){
 var m_main = b4w.require("main");
 var m_data = b4w.require("data");
 var m_scene = b4w.require("scenes");
 var m_config = b4w.require("config");
-var m_debug = b4w.require("debug");
-var a_trans = b4w.require("transform");
+var m_trans = b4w.require("transform");
 var m_camera = b4w.require("camera");
+var m_controls = b4w.require("controls");
+var m_material = b4w.require("material");
+var m_rgba = b4w.require("rgba");
 var canvas = document.getElementById("container");
-cube_test.core.loaded_cb = ((function (m_main,m_data,m_scene,m_config,m_debug,a_trans,m_camera,canvas){
+var position = cljs.core.atom.call(null,new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"current","current",-1088038603),(0),new cljs.core.Keyword(null,"direction","direction",-633359395),(1),new cljs.core.Keyword(null,"back-limit","back-limit",-289535030),(30),new cljs.core.Keyword(null,"front-limit","front-limit",-271127864),(-30)], null));
+var cube_range = cljs.core.range.call(null,(-30),(30));
+cube_test.core.loaded_cb = ((function (m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range){
 return (function cube_test$core$start_$_loaded_cb(data_id,success){
-return console.log("data id, success are: ",data_id,success);
-});})(m_main,m_data,m_scene,m_config,m_debug,a_trans,m_camera,canvas))
+return console.log("Main scene loaded on thread number: ",data_id,"with a success value of: ",success);
+});})(m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range))
 ;
 
-cube_test.core.stageload_cb = ((function (m_main,m_data,m_scene,m_config,m_debug,a_trans,m_camera,canvas){
-return (function cube_test$core$start_$_stageload_cb(data_id,success){
-if(cljs.core.truth_(m_data.is_primary_loaded())){
+cube_test.core.manifold_cb = ((function (m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range){
+return (function cube_test$core$start_$_manifold_cb(obj,manifold_id){
+
 var current_camera = m_scene.get_active_camera();
-var cube = m_scene.get_object_by_name("Weird");
-return m_camera.translate_view(current_camera,(1),(1),(-10));
+var cube = m_scene.get_object_by_name("Cube");
+var sensor_data = cljs.core.first.call(null,obj);
+var mat_name = cljs.core.first.call(null,m_material.get_materials_names(cube));
+if((cljs.core._EQ_.call(null,new cljs.core.Keyword(null,"current","current",-1088038603).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position)),new cljs.core.Keyword(null,"back-limit","back-limit",-289535030).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position)))) || (cljs.core._EQ_.call(null,new cljs.core.Keyword(null,"current","current",-1088038603).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position)),new cljs.core.Keyword(null,"front-limit","front-limit",-271127864).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position))))){
+cljs.core.swap_BANG_.call(null,position,cljs.core.assoc,new cljs.core.Keyword(null,"direction","direction",-633359395),((-1) * new cljs.core.Keyword(null,"direction","direction",-633359395).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position))));
+} else {
+}
+
+cljs.core.swap_BANG_.call(null,position,cljs.core.assoc,new cljs.core.Keyword(null,"current","current",-1088038603),(new cljs.core.Keyword(null,"direction","direction",-633359395).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position)) + new cljs.core.Keyword(null,"current","current",-1088038603).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position))));
+
+m_material.set_diffuse_color(cube,mat_name,m_rgba.from_values((cljs.core.rand_int.call(null,(10)) / (10)),(cljs.core.rand_int.call(null,(10)) / (10)),(cljs.core.rand_int.call(null,(10)) / (10))));
+
+m_trans.set_translation(cube,(0),(1),(new cljs.core.Keyword(null,"current","current",-1088038603).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,position)) / (10)));
+
+return m_trans.rotate_y_local(cube,sensor_data);
+});})(m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range))
+;
+
+cube_test.core.stageload_cb = ((function (m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range){
+return (function cube_test$core$start_$_stageload_cb(data_id,success){
+
+if(cljs.core.truth_(m_data.is_primary_loaded())){
+var elapsed_sensor = m_controls.create_elapsed_sensor();
+m_controls.create_sensor_manifold(null,"m_main",m_controls.CT_CONTINUOUS,cljs.core.clj__GT_js.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [elapsed_sensor], null)),cube_test.core.manifold_cb);
+
+return console.log("stage-load callback finished");
 } else {
 return null;
 }
-});})(m_main,m_data,m_scene,m_config,m_debug,a_trans,m_camera,canvas))
+});})(m_main,m_data,m_scene,m_config,m_trans,m_camera,m_controls,m_material,m_rgba,canvas,position,cube_range))
 ;
 
-m_data.load("cube_test.json",cube_test.core.loaded_cb,cube_test.core.stageload_cb,true,false);
+m_config.set("console_verbose",true);
+
+m_data.load([cljs.core.str(json)].join(''),cube_test.core.loaded_cb,cube_test.core.stageload_cb,true,false);
 
 return m_main.init(canvas);
 });
